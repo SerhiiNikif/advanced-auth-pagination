a406e66991c60d160a1e0982ed0fdc6df4b36449
Serhii <nikiforenko.sergiy@gmail.com>
2023-07-23 19:05:50 +0300

    Added route put, get category/:id/product, query params, certain error handling to swagger, added README.md

---
+++ README.md
@@ -0,0 +1,55 @@
+# rest-api-mongo-swagger
+
+# How to use
+
+## 1. Clone Project into your local machine
+```
+git clone https://github.com/SerhiiNikif/rest-api-mongo-swagger.git
+```
+
+## 2. Go into project folder
+
+```
+cd rest-api-mongo-swagger
+```
+
+## 3. Setting environment file .env.
+Create an `.env` file in the root of the project and fill it with the values ​​from the `.env.sample` file.
+
+```
+MONGO_URI=mongodb://localhost:27017/db
+PORT=4000
+BCRYPT_SALT=10
+JWT_SECRET=SECRET_VALUE
+PBANK_API_URL=https://api.privatbank.ua/p24api/pubinfo
+```
+
+## 4. Start project
+
+```
+npm install
+```
+
+```
+npm run dev
+```
+
+## 5. Play with APIs now !
+> Please make sure mongoDB Server service is installed and running on your localhost:27017.
+
+Now, you are ready to test all APIs.
+Just simply open your browser http://localhost:4000/api-docs.
+
+## APIs Authorization
+
+> All APIs are protected by accessToken (JWT).
+
+### When calling these protected APIs, make sure you add %BearerToken% in `Authorization` request Header.
+
+```
+Authorization: Bearer <accessToken>
+```
+
+### How to get accessToken ?
+
+When user login sucessfully, an unique accessToken will be returned.
--- src/helpers/sort.js
+++ src/helpers/sort.js
@@ -1,23 +1,19 @@
 import { createError } from "../helpers/index.js";
 
 const sortByURLParams = async (arrForAggregate, attribute, sort) => {
-    try {
-        if (typeof attribute !== 'undefined' && ['price', 'createdate'].includes(attribute.toLowerCase()) &&
-            typeof sort !== 'undefined' && ['asc', 'desc'].includes(sort.toLowerCase())
-        ) {
-            // if attribute and sort are specified
-            arrForAggregate.push({ $sort: {[attribute]: sort === 'desc' ? -1 : 1} });
-        } else if (typeof attribute === 'undefined' && typeof sort === 'undefined') {
-            // skip if attribute and sort are not specified
-        } else {
-            /*
-                If the parameter 'attribute' was specified without 'sort', or vice versa, 
-                or if these parameters were specified incorrectly
-            */
-            throw createError(422, "You specified the 'attribute' and 'sort' parameters incorrectly");
-        }
-    } catch (error) {
-        console.log(error.message);
+    if (typeof attribute !== 'undefined' && ['price', 'createdate'].includes(attribute.toLowerCase()) &&
+        typeof sort !== 'undefined' && ['asc', 'desc'].includes(sort.toLowerCase())
+    ) {
+        // if attribute and sort are specified
+        arrForAggregate.push({ $sort: {[attribute]: sort === 'desc' ? -1 : 1} });
+    } else if (typeof attribute === 'undefined' && typeof sort === 'undefined') {
+        // skip if attribute and sort are not specified
+    } else {
+        /*
+            If the parameter 'attribute' was specified without 'sort', or vice versa,
+            or if these parameters were specified incorrectly
+        */
+        throw createError(422, "You specified the 'attribute' and 'sort' parameters incorrectly");
     }
 }
 
