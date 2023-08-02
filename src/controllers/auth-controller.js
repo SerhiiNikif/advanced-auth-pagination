e2d846940521eb6d1af6f53d6b0017324587adca
Serhii <nikiforenko.sergiy@gmail.com>
2023-08-02 17:11:07 +0300

    Account activation with confirmation email

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -37,9 +37,12 @@ class UserController {
 
     async activate(req, res, next) {
         try {
+            const activationLink = req.params.link;
+            await userService.activate(activationLink);
 
+            return res.redirect(process.env.CLIENT_URL);
         } catch (e) {
-            next(e);
+            console.log(e);
         }
     }
 
--- src/services/auth/user.service.js
+++ src/services/auth/user.service.js
@@ -10,21 +10,28 @@ import ApiError from '../../exceptions/api-error.js';
 class UserService {
     async registration(email, password) {
         const candidate = await UserModel.findOne({email});
-
         if (candidate) {
-            throw ApiError.BadRequest(`User with email address ${email} already exists`)
+            throw ApiError.BadRequest(`User with email address ${email} already exists`);
         }
-
         const hashPassword = await bcrypt.hash(password, 3);
         const activationLink = uuidv4(); // v34fa-asfasf-142saf-sa-asf
         const user = await UserModel.create({email, password: hashPassword, activationLink});
-        await mailService.sendActivationMail(email, activationLink);
+        await mailService.sendActivationMail(email, `${process.env.API_URL}/auth/activate/${activationLink}`);
 
         const userDto = new UserDto(user);
         const tokens = tokenService.generateTokens({...userDto});
         await tokenService.saveToken(userDto.id, tokens.refreshToken);
 
         return {...tokens, user: userDto}
+    }
+
+    async activate(activationLink) {
+        const user = await UserModel.findOne({activationLink});
+        if (!user) {
+            throw ApiError.BadRequest(`Incorrect activation link`);
+        }
+        user.isActivated = true;
+        await user.save();
     }
 }
 
